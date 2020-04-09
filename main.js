5452f010efdbe23320087987db25917dbb034d92
maather-alshablan <63198807+maather-alshablan@users.noreply.github.com>
2020-04-09 18:44:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function signup(){
+    <a   href="AboutUs.html">About</a>
+}
+function restpass(){
+    document.getElementById("popup1").style.display ="none";
+    swal("Please wait!","We will send you a password recovery email...");
+}
+function check(){
+    document.getElementById("popup1").style.display ="block";
+}
